1f6f7cae10fa3ebb19613461e062c6b40e627829
parveenyadav <parveenyadavtitian@gmail.com>
2021-11-15 01:21:14 +0530

    nuxt.js

---
+++ vue/nuxt.js
@@ -0,0 +1,7 @@
+NUXT.JS
+
+create nuxt App
+	npx create-nuxt-app appName
+Installing tailwind CSS
+	https://tailwindcss.com/docs/guides/nuxtjs
+	<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
